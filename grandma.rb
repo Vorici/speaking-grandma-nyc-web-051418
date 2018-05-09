e2217c920c11f765d8f889119410043237601e0f
Ledio Bode <l.bode515@gmail.com>
2018-05-09 19:27:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase
   "HUH?! SPEAK UP, SONNY!."
   
-  elseif phrase == phrase.upcase
+  else if phrase == phrase.upcase
   "NO, NOT SINCE 1938!"
   
   else phrase == "I LOVE YOU GRANDMA!"
